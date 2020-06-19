ca07c74d33fcb2a52a4546519e3702450c8a2e9d
monicajoa <mnortiz.ortiz@gmail.com>
2020-06-19 01:12:27 -0500

    0x1B. C - Sorting algorithms & Big O, task 4. Shell sort - Knuth Sequence, add file and main

---
+++ 100-shell_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+/**
+ * shell_sort - sorts an array of integers using shell sort algorithm
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: Void
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, ref;
+	int aux, idx;
+
+	if (array == NULL)
+	{
+		return;
+	}
+	gap = 0;
+	while (gap < size / 3)
+	{
+		gap = gap * 3 + 1;
+	}
+	while (gap > 0)
+	{
+		ref = gap;
+		while (ref < size)
+		{
+			idx = (int)ref - (int)gap;
+			while (idx >= 0)
+			{
+				if (array[ref] < array[idx])
+				{
+					aux = array[ref];
+					array[ref] = array[idx];
+					array[idx] = aux;
+					ref = ref - gap;
+				}
+				idx = idx - (int)gap;
+			}
+			ref = ref + 1;
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
+}
